24055e48e0078e3dd1f9eff28b20d87367b40e36
KaderKivrak <119049854+KaderKivrak@users.noreply.github.com>
2023-12-07 11:44:23 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function attemptLogin() {
+    // Get the values entered by the user
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    if (username === "karzan" && password === "magnus") {
+    
+    location.replace("sikkerhedslog.html")
+        } else {
+            document.getElementById("loginMessage").innerHTML = "Invalid username or password";
+        }
+    }
+    
+        // Logout Function
+    function logout() {
+        location.replace("index.html")
+    }
